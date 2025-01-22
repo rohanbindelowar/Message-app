2fc0fa54ff67218cdc7b411083e1b0a4ac48a918
rohanbindelowar <rohanbindelowar@gmail.com>
2025-01-22 09:57:37 -0500

    arts component done

--- src/components/Arts.js
+++ src/components/Arts.js
@@ -1,11 +1,18 @@
 import React from "react";
 import CreateNewMessage from "./CreateNewMessage";
+import MessageBox from "./MessageBox";
 
-const Arts = () => {
+const Arts = (props) => {
+  const artsArr = props.data.filter((item)=> item.boards["arts"])
   return (
     <div>
       <CreateNewMessage />
-      Arts
+      <h2 className="title">Arts Message Box!</h2>
+      {artsArr.length === 0 ? (
+        <h3 className="emptyMessage">There is no message! Create a new message.</h3>
+      ) : (
+        artsArr.map((item, i) => <MessageBox key={i} item={item} />)
+      )}
     </div>
   );
 };
